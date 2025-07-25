bf10b7b900140abd31f9a9767de3daa4f100ed77
rajeabhi <abhishek-rajput@hotmail.com>
2025-07-26 00:02:40 +0530

    Hashcode changed

--- HeroesVillainsExample/HeroesVillainsExample/obj/Debug/net8.0/HeroesVillainsExample.AssemblyInfo.cs
+++ HeroesVillainsExample/HeroesVillainsExample/obj/Debug/net8.0/HeroesVillainsExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b1acb616fe580efb320f4c3f449d963a09fecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab25c47de8504e4166499b6ae706a4dc5038ecd")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
