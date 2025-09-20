3b229617e8c7a47ecf86923f97cca842d2e904dc
rajeabhi <abhishek-rajput@hotmail.com>
2025-09-20 21:49:36 +0530

    Update checked

--- HeroesVillainsExample/HeroesVillainsExample/obj/Debug/net8.0/HeroesVillainsExample.AssemblyInfo.cs
+++ HeroesVillainsExample/HeroesVillainsExample/obj/Debug/net8.0/HeroesVillainsExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bab25c47de8504e4166499b6ae706a4dc5038ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf10b7b900140abd31f9a9767de3daa4f100ed77")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeroesVillainsExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
